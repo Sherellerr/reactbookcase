1d43cc4db17ffaa7d377e80fd62812d087e9ddfa
Sherellerr <68772697+Sherellerr@users.noreply.github.com>
2020-10-07 14:53:03 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+ import Book from "./components/Book.js";
+ import data from './models/books.json';
+ const App = (props) => {
+   const [books] = useState(data);
+   return (
+       <div>
+           {books.map(book => <Book key={book.id} book={book}/>)}
+       </div>
+); }
+export default App;
